75f34f654fb09076860793156cbecf5149d26552
Wim van Arkel <warkel@me.com>
2017-11-06 10:33:49 +0100

    Seque and back button working

--- I Am Poor/poorDetailVC.swift
+++ I Am Poor/poorDetailVC.swift
@@ -8,27 +8,13 @@
 import UIKit
 
 class poorDetailVC: UIViewController {
-
+    @IBOutlet weak var backToPoorBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func backToPoorPressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
